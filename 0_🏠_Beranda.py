b7b80f47c2113955e029fc4f3b0a4d5f92a2a2ae
litarahmasadina <litarahmasadina@gmail.com>
2026-05-31 00:28:04 +0700

    Update 0_🏠_Beranda.py

--- 0_🏠_Beranda.py
+++ 0_🏠_Beranda.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Trashify Capstone CC26-PSU346", page_icon="♻️", layout="wide")
 
-st.title("♻️ Trashify: Solusi Pemilahan Sampah Cerdas")
+st.title("♻️ Trashify : Smart Waste Classification & Guide")
 st.write("""
 Selamat datang di aplikasi **Trashify**.
 Aplikasi ini dikhususkan untuk memenuhi **capstone project CC26-PSU346** dengan tujuan mengklasifikasikan gambar sampah ke dalam tiga kategori utama: **Organik, Anorganik, dan Residu**.
